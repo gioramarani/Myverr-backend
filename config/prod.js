c37474f487433c0273044715a43722260ccbc52d
Giora Marani <giora.marani@gmail.com>
2023-08-04 18:15:16 +0300

    dont use env.

--- config/prod.js
+++ config/prod.js
@@ -7,15 +7,15 @@ dotenv.config()
 //   dbName : 'tester_db'
 // }
 
-// export default {
-//   dbURL: 'mongodb+srv://gioramarani:1234@cluster0.pdpj63k.mongodb.net/',
-//   dbName : 'Myverrdb'
-// }
-
 export default {
-  dbURL: process.env.ATLAS_URL,
-  dbName : process.env.ATLAS_DB_NAME,
+  dbURL: 'mongodb+srv://gioramarani:1234@cluster0.pdpj63k.mongodb.net/',
+  dbName : 'Myverrdb'
 }
+
+// export default {
+//   dbURL: process.env.ATLAS_URL,
+//   dbName : process.env.ATLAS_DB_NAME,
+// }
 
 // export default {
 //   dbURL: process.env.,
